1fc81b84c588c1d07e8922c0fdd01d8d9e95fe36
Tero Yrjola <yrjola.tero@gmail.com>
2018-08-17 22:58:23 +0300

    Update MultiYoutubePlayer.js
    Fix unnecessary console log

--- src/components/YoutubePlayer/MultiYoutubePlayer.js
+++ src/components/YoutubePlayer/MultiYoutubePlayer.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 class MultiYoutubePlayer extends Component {
     render() {
         const videoIDs = Array.from(Array(20)).map(() => this.props.generateRandomVideoID());
-        console.log(videoIDs)
         return (
             <div style={{width: '100%', height: '100%'}}>
                 {videoIDs.map((id, index) => ( <div style={{width: '20%', height:'25%', display: 'inline-block'}}>
