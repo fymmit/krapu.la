d50d22e8cb5278148fa6af2cfbe7a834fd9c1fed
fymmit <blomqvist.timo@gmail.com>
2018-08-17 21:57:41 +0300

    Re-add LastKrappe to the view.

--- src/views/KrapulaView.js
+++ src/views/KrapulaView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import {Grid, Row, Col} from 'react-bootstrap'
 import NameGenerator from '../components/NameGenerator';
+import LastKrappe from '../components/LastKrappe';
 
 const KrapulaView = () => (
     <div>
@@ -11,6 +12,9 @@ const KrapulaView = () => (
                 <Col lg={12} md={12} sm={12} xs={12}>
                     <NameGenerator type="krapula"/>
                 </Col>
+            </Row>
+            <Row>
+                <LastKrappe/>
             </Row>
         </Grid>
     </div>
